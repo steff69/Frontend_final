5ee7afa4f1cf1a949c12f7da1a7f5ace4e8c1bee
Mustapha <mustapha.aouadi2@gmail.com>
2024-10-01 22:47:22 +0300

    first commit

--- lib/controller/LoginController.dart
+++ lib/controller/LoginController.dart
@@ -13,7 +13,9 @@ import 'package:get_storage/get_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:travel_app/Models%202/userModel.dart';
 import 'package:travel_app/constants/constants.dart';
+import 'package:travel_app/login/login.dart';
 import 'package:travel_app/pages/MainScreen.dart';
+import 'package:travel_app/pages/ProfilePage.dart';
 
 class LoginController extends GetxController {
   //RxList<DatumC> dataList = RxList<DatumC>();
@@ -53,10 +55,11 @@ class LoginController extends GetxController {
             backgroundColor: kPrimary,
             icon: Icon(Ionicons.fast_food_outline));
 
-        Timer.periodic(Duration(seconds: 1), (timer) {
-          loading.value = false;
-          pf.value = true;
-        });
+        loading.value = false;
+        pf.value = true;
+
+        Get.off(() => ProfilePage(),
+            transition: Transition.fade, duration: Duration(milliseconds: 900));
       }
       if (response.statusCode == 400) {
         String text = jsonDecode(response.body)["message"];
@@ -80,5 +83,10 @@ class LoginController extends GetxController {
 
   void logout() {
     box.erase();
+    pf.value = false;
+
+    print(pf.value);
+    Get.off(() => LoginPage(),
+        transition: Transition.fade, duration: Duration(milliseconds: 900));
   }
 }
